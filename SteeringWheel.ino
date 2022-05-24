1d07540c44cf8f50cacd838cbedcf8584848818d
Inteltank <40775083+inteltank1@users.noreply.github.com>
2022-05-24 21:59:50 +0200

    added snake and other things

--- SteeringWheel.ino
+++ SteeringWheel.ino
@@ -20,30 +20,49 @@ int subir = subir_old;
 int finradio = 0;
 int on = 0;
 int timer = 20;
+int cuantosubir = 15;
+const int X_pin = 0;
+const int Y_pin = 1;
 
 #include "pitches.h"
 
 int melody[] = {
   NOTE_C5, NOTE_G4, NOTE_D5, NOTE_C5};
-
 int yaesta[] = {
   NOTE_C5, NOTE_C5};
 
-int marchupbut = 44;
-int marchdownbut = 46;
+int startupmusic[] = {
+  NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4, NOTE_A4, NOTE_F4};
 
-int rvupbut = 50;
+
+int marchupbut = 53;
+int marchdownbut = 52;
+
+int rvupbut = 34;
 int rvdownbut = 48;
 
-int embraguebut = 52;
+
+int embraguebut = 50;
 int embraguecont = 0;
 
-int radiobut = 53;
+int radiobut = 51;
 int duration = 100;
 
 #include "LedControl.h"
 
 LedControl lc=LedControl(4,2,3,1);
+
+//snaek
+const int screenWidth = 8;
+const int screenHeight = 8;
+int snakeX, snakeY, foodX, foodY, score = 0, snakeSize = 1;
+char direction;
+int tailX[100], tailY[100];
+bool isGameOver = false;
+int snakeempezar = 0;
+//end snaek
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -63,10 +82,9 @@ void setup() {
   pinMode(embraguebut, INPUT_PULLUP);
   pinMode(6, OUTPUT); 
 
-
     for (int lights = 0; lights < 15; lights++) {
-      pinMode(36-lights, LOW);
       pinMode(36-lights, OUTPUT);
+      digitalWrite(36-lights, LOW);
     } 
     lcd.begin(16, 2);
     lcd.print("RPM: ");
@@ -76,7 +94,8 @@ void setup() {
 void startup() {
       for (int lights = 0; lights < 15; lights++) {
       digitalWrite(36-lights, HIGH);
-      delay(50);
+        tone(13, startupmusic[lights], 100);    
+      delay(100);
     } 
     for (int cuanto = 0; cuanto < 4; cuanto++) {
       for (int lights = 0; lights < 15; lights++) {
@@ -106,7 +125,7 @@ void writeArduinoOnMatrix(int i) {
                 {0x0, 0x7e, 0x4, 0x8, 0x3c, 0x8, 0x8, 0x0},
                 {0x0, 0x3c, 0x42, 0x42, 0x3c, 0x42, 0x3c, 0x0},
                 {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}};
-                
+                 
   lc.setRow(0,0,n[i][0]);
   lc.setRow(0,1,n[i][1]);
   lc.setRow(0,2,n[i][2]);
@@ -143,7 +162,7 @@ void embrague() {
         marcha = 1;
         on = 0;
         lcd.noDisplay();
-        lcd.clear();
+        lcd.home();
         Serial.print("embragueoff");
         lc.shutdown(0,true);
         digitalWrite(6, LOW); 
@@ -162,10 +181,11 @@ void embrague() {
 
 void radiocosa() {
 
-   if (digitalRead(radiobut) == LOW  && (finradio == 0)) {
+    if (digitalRead(radiobut) == LOW  && (finradio == 0)) {
       if (radio == 0) {
         radio = 1;
         finradio = 1;
+        delay(1000);
       for (int thisNote = 0; thisNote < 4; thisNote++) {
       delay(10);
       tone(13, melody[thisNote], duration);    
@@ -176,6 +196,7 @@ void radiocosa() {
       if (radio == 0) {
       radio = 1;
       finradio = 0;
+      delay(1000);
       for (int thisNote = 0; thisNote < 2; thisNote++) {
       delay(10);
       tone(13, yaesta[thisNote], duration);    
@@ -198,7 +219,7 @@ void marchaup() {
         if (marcha<9) {
           marcha++;
           rev=rev-5000;
-          subir=subir-15;
+          subir=subir-cuantosubir;
         }
 
       } 
@@ -219,7 +240,7 @@ void marchadown() {
           marcha--; 
           if (marcha > 1)
           rev=rev+3000;
-          subir=subir+10;
+          subir=subir+cuantosubir;
         }
       } 
     } else if (digitalRead(marchdownbut) == HIGH) {
@@ -243,6 +264,11 @@ void led() {
 
 void loop() {
 
+Serial.print("X_pin: ");
+Serial.print(analogRead(X_pin));
+Serial.print("  Y_pin: ");
+Serial.print(analogRead(Y_pin));
+Serial.println("");
 if (debug == 0) {
 
 embrague();
@@ -263,11 +289,13 @@ marchaup();
 marchadown();
 
 // Subir y bajar revoluciones
-  if (digitalRead(rvdownbut) == LOW) {
+//  if (digitalRead(rvdownbut) == LOW) {
+  if (analogRead(X_pin) < 200) {
         rev=rev-subir;
   }
 
-  if (digitalRead(rvupbut) == LOW) {
+//  if (digitalRead(rvupbut) == LOW) {
+  if (analogRead(X_pin) > 800) {
         rev=rev+subir;
   }
 radiocosa();
@@ -323,7 +351,7 @@ if (roto == 1) {
 }
 
 // Otra version para cualquier easter egg
-  if (digitalRead(rvdownbut) == LOW && digitalRead(rvupbut) == LOW && digitalRead(marchupbut) == LOW && digitalRead(marchdownbut) == LOW && digitalRead(radiobut) == LOW) {
+  if (digitalRead(embraguebut) == LOW && digitalRead(marchupbut) == LOW && digitalRead(marchdownbut) == LOW && digitalRead(radiobut) == LOW) {
     
     if (debug == 0) {
     startup();
@@ -331,8 +359,23 @@ if (roto == 1) {
     } else if (debug == 1) {
     startup();
     debug = 0;
+    marcha = 1;
     }
   }
+
+//SNAEK EMPEZAMIENTO
+if (snakeempezar == 1) {
+  if (isGameOver) {
+    playGameOverSong();
+    showGameOverScreen();
+    startup();
+    snakeempezar = 0;
+    lc.clearDisplay(0);
+  } else {
+    startGame();
+  }
+} 
+//SNAEK FINALIZAMIENTO
 
 //DEBUG MODE
   if (debug == 1) {
@@ -340,22 +383,184 @@ if (roto == 1) {
     //KNIGHT RIDER MODE
     if (digitalRead(marchdownbut) == LOW) {
 
-      for (int count=0;count<15;count++) {
+      for (int count=0;count<18;count++) {
       digitalWrite(todaslasluces[count], HIGH);
       delay(timer);
-      digitalWrite(todaslasluces[count-2], LOW);
+      digitalWrite(todaslasluces[count-3], LOW);
       delay(timer);
+      tone(13, count * 50, 50);  
       }
-      for (int count=15;count>=0;count--) {
+      
+      for (int count=17;count>=-3;count--) {
       digitalWrite(todaslasluces[count], HIGH);
       delay(timer);
-      digitalWrite(todaslasluces[count+2], LOW);
+      digitalWrite(todaslasluces[count+3], LOW);
       delay(timer);
+      tone(13, count * 50, 50);  
       }
+      tone(13, 0, 50);
 
-    }
-
+    } else if (digitalRead(marchupbut) == LOW && snakeempezar == 0)  {
+      snakeempezar = 1;
+    setupPins();
+    setupLedBoard();
+    setupSnakePosition();
+    setupFoodPosition();
+    } 
   }
 }
 
+void setupSnakePosition() {
+  snakeX = 4;
+  snakeY = 4;
+}
+void setupFoodPosition() {
+  foodX = rand() % screenWidth;
+  foodY = rand() % screenHeight;
+}
+void setupLedBoard() {
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 1);
+  lc.clearDisplay(0);
+}
+void setupPins() {
+}
+void playGameOverSong() {
+  tone(13, 1000, 100);
+  delay(100);
+  tone(13, 2000, 100);
+  delay(100);
+  tone(13, 3000, 100);
+  delay(100);
+  tone(13, 3500, 100);
+  delay(100);
+  tone(13, 3000, 100);
+}
+void playFoodEatenSong() {
+  tone(13, 500, 100);
+}
+void startGame() {
+  manageGameOver();
+  setJoystickDirection();
+  changeSnakeDirection();
+  manageSnakeOutOfBounds();
+  manageEatenFood();
+  manageSnakeTailCoordinates();
+  drawSnake();
+  delay(100);
+}
+void manageGameOver() {
+  for (int i = 1; i < snakeSize; i++) {
+    if (tailX[i] == snakeX && tailY[i] == snakeY) {
+      isGameOver = true;
+    }
+  }
+}
+void manageSnakeOutOfBounds() {
+  if (snakeX >= screenWidth) {
+    snakeX = 0;
+  } else if (snakeX < 0) {
+    snakeX = screenWidth - 1;
+  }
+  if (snakeY >= screenHeight) {
+    snakeY = 0;
+  } else if (snakeY < 0) {
+    snakeY = screenHeight - 1;
+  }
+}
+void manageSnakeTailCoordinates() {
+  int previousX, previousY, prevX, prevY;
+  previousX = tailX[0];
+  previousY = tailY[0];
+  tailX[0] = snakeX;
+  tailY[0] = snakeY;
 
+  for (int i = 1; i < snakeSize; i++) {
+    prevX = tailX[i];
+    prevY = tailY[i];
+    tailX[i] = previousX;
+    tailY[i] = previousY;
+    previousX = prevX;
+    previousY = prevY;
+  }
+}
+void manageEatenFood() {
+  if (snakeX == foodX && snakeY == foodY) {
+    playFoodEatenSong();
+    score++;
+    snakeSize++;
+    setupFoodPosition();
+  }
+}
+void setJoystickDirection() {
+  if (analogRead(X_pin) > 1000) {
+    direction = 'u';
+  } else if (analogRead(X_pin) < 100) {
+    direction = 'd';
+  } else if (analogRead(Y_pin) > 1000) {
+    direction = 'l';
+  } else if (analogRead(Y_pin) < 100) {
+    direction = 'r';
+  }
+}
+void changeSnakeDirection() {
+  switch (direction) {
+    case 'l':
+      snakeX--;
+      break;
+    case 'r':
+      snakeX++;
+      break;
+    case 'u':
+      snakeY--;
+      break;
+    case 'd':
+      snakeY++;
+      break;
+  }
+}
+void showGameOverScreen() {
+  for (int i = 0; i < screenHeight; i++) {
+    for (int j = 0; j < screenWidth; j++) {
+      showLed(j, i);
+      delay(50);
+    }
+  }
+  resetVariables();
+}
+void resetVariables() {
+  setupSnakePosition();
+  setupFoodPosition();
+  direction = ' ';
+  isGameOver = false;
+  score = 0;
+  snakeSize = 1;
+}
+void showLed(int row, int column) {
+  lc.setLed(0, row, column, true);
+}
+void hideLed(int row, int column) {
+  lc.setLed(0, row, column, false);
+}
+void drawSnake() {
+  for (int i = 0; i < screenHeight; i++) {
+    for (int j = 0; j < screenWidth; j++) {
+      if (i == snakeY && j == snakeX) {
+        showLed(snakeX, snakeY);
+      } else if (i == foodY && j == foodX) {
+        showLed(foodX, foodY);
+      } else {
+        bool isShown = false;
+        for (int k = 0; k < snakeSize; k++) {
+          if (tailX[k] == j && tailY[k] == i) {
+            showLed(j, i);
+            isShown = true;
+          }
+        }
+        if (!isShown) {
+          hideLed(j, i);
+        }
+      }
+    }
+  }
+}
